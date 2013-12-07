a1aeb93fcb2fc48948a7407f028f7bf7c86f33ef
Zac <zeeveener@gmail.com>
2013-12-06 20:00:58 -0700

    Initial Commit
    The skelly has been uploaded

---
+++ src/main/java/com/zeeveener/tenjavacontest/TenJava.java
@@ -0,0 +1,14 @@
+package com.zeeveener.tenjavacontest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenJava extends JavaPlugin{
+	
+	public void onEnable(){
+		
+	}
+	public void onDisable(){
+		
+	}
+	
+}
