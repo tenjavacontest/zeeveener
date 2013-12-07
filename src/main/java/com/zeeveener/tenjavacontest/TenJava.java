01e0b76d5cbaf51fb32f92ebb330ef4fa2ecf0c4
Zac <zeeveener@gmail.com>
2013-12-07 12:23:43 -0700

    Add Lobby Class

--- src/main/java/com/zeeveener/tenjavacontest/TenJava.java
+++ src/main/java/com/zeeveener/tenjavacontest/TenJava.java
@@ -4,16 +4,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.zeeveener.tenjavacontest.listeners.Battle;
 import com.zeeveener.tenjavacontest.listeners.Login;
+import com.zeeveener.tenjavacontest.logic.Lobby;
 import com.zeeveener.tenjavacontest.utilities.Chat;
 import com.zeeveener.tenjavacontest.utilities.Config;
 
 public class TenJava extends JavaPlugin{
 	
 	public Config config;
+	public Lobby lobby;
 	
 	public void onEnable(){
 		new Chat(this);
 		config = new Config(this);
+		lobby = new Lobby(this);
+		
 		
 		getServer().getPluginManager().registerEvents(new Battle(), this);
 		getServer().getPluginManager().registerEvents(new Login(this), this);
@@ -21,6 +25,6 @@ public class TenJava extends JavaPlugin{
 		Chat.toConsole("You're ready to go!");
 	}
 	public void onDisable(){
-		
+		this.getServer().getScheduler().cancelTasks(this);
 	}
 }
---
+++ src/main/java/com/zeeveener/tenjavacontest/logic/Lobby.java
@@ -0,0 +1,66 @@
+package com.zeeveener.tenjavacontest.logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
+import com.zeeveener.tenjavacontest.TenJava;
+import com.zeeveener.tenjavacontest.utilities.Chat;
+
+public class Lobby {
+
+	private TenJava plugin;
+	private static List<Player> inLobby = new ArrayList<Player>();
+	private static HashMap<Player, PlayerInventory> oldInvs = new HashMap<Player, PlayerInventory>();
+	private Location spawn;
+	
+	public Lobby(TenJava instance){
+		plugin = instance;
+		getDefaultSpawn();
+	}
+	
+	private void getDefaultSpawn(){
+		World w = plugin.getServer().getWorld(plugin.config.getConfig().getString("Game.Lobby.world", "world"));
+		double x = plugin.config.getConfig().getInt("Game.Lobby.x", 0);
+		double y = plugin.config.getConfig().getInt("Game.Lobby.y", 0);
+		double z = plugin.config.getConfig().getInt("Game.Lobby.z", 0);
+		spawn = new Location(w, x, y, z);
+	}
+	public Location getSpawn(){
+		return spawn;
+	}
+	public void setSpawn(Location loc){
+		spawn = loc;
+		plugin.config.getConfig().set("Game.Lobby.world", loc.getWorld().getName());
+		plugin.config.getConfig().set("Game.Lobby.x", loc.getX());
+		plugin.config.getConfig().set("Game.Lobby.y", loc.getY());
+		plugin.config.getConfig().set("Game.Lobby.z", loc.getZ());
+	}
+	
+	public void joinLobby(Player p){
+		oldInvs.put(p, p.getInventory());
+		p.getInventory().clear();
+		p.teleport(spawn);
+		Chat.message(p, "Welcome to the Battle Lobby!");
+		Chat.message(p, "To start a game, type /start");
+	}
+	public void leaveLobby(Player p){
+		p.getInventory().clear();
+		p.getInventory().setContents(oldInvs.get(p).getContents());
+	}
+	
+	public boolean isPlayerInLobby(Player p){
+		return inLobby.contains(p);
+	}
+	public void addToLobby(Player p){
+		inLobby.add(p);
+	}
+	public void removeFromLobby(Player p){
+		inLobby.remove(p);
+	}
+}
